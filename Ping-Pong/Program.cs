3f6aa6c418eed078a4ede952ebf3fcc42a71870f
zbl14 <ifthereisoneday@gmail.com>
2022-07-10 15:18:21 -0700

    add branching and for loop to print ping, pong and ping-pong as the rule required

---
+++ Ping-Pong/Program.cs
@@ -0,0 +1,35 @@
+using System;
+// using System.Collections.Generic;
+// using Ping-Pong.Models;
+
+// namespace Ping-Pong
+// {
+  public class Program
+  {
+    public static void Main()
+    {
+      Console.WriteLine("Please enter a number");
+      string strCountUpto = Console.ReadLine();
+      int countUpTo = int.Parse(strCountUpto);
+
+      for (int i = 1; i <= countUpTo; i++)
+      {
+        if (i % 3 == 0 && i % 5 == 0)
+        {
+          Console.WriteLine("ping-pong");
+        }
+        else if (i % 3 == 0)
+        {
+          Console.WriteLine("ping");
+        }
+        else if (i % 5 ==0)
+        {
+          Console.WriteLine("pong");
+        }
+        else {
+          Console.WriteLine(i);
+        }
+      }
+    }  
+  }
+// }
