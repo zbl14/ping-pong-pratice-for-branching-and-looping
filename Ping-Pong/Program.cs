7c4fecd20bd67d4283cd499b725767ee401b7d4d
zbl14 <ifthereisoneday@gmail.com>
2022-07-10 15:27:28 -0700

    remove comment

--- Ping-Pong/Program.cs
+++ Ping-Pong/Program.cs
@@ -1,36 +1,31 @@
 using System;
-// using System.Collections.Generic;
-// using Ping-Pong.Models;
 
-// namespace Ping-Pong
-// {
-  public class Program
+public class Program
+{
+  public static void Main()
   {
-    public static void Main()
-    {
-      Console.WriteLine("Please enter a number");
-      string strCountUpto = Console.ReadLine();
-      int countUpTo = int.Parse(strCountUpto);
-      Console.WriteLine("----------------------");
+    Console.WriteLine("Please enter a number");
+    string strCountUpto = Console.ReadLine();
+    int countUpTo = int.Parse(strCountUpto);
+    Console.WriteLine("----------------------");
 
-      for (int i = 1; i <= countUpTo; i++)
+    for (int i = 1; i <= countUpTo; i++)
+    {
+      if (i % 3 == 0 && i % 5 == 0)
       {
-        if (i % 3 == 0 && i % 5 == 0)
-        {
-          Console.WriteLine("ping-pong");
-        }
-        else if (i % 3 == 0)
-        {
-          Console.WriteLine("ping");
-        }
-        else if (i % 5 ==0)
-        {
-          Console.WriteLine("pong");
-        }
-        else {
-          Console.WriteLine(i);
-        }
+        Console.WriteLine("ping-pong");
       }
-    }  
-  }
-// }
+      else if (i % 3 == 0)
+      {
+        Console.WriteLine("ping");
+      }
+      else if (i % 5 ==0)
+      {
+        Console.WriteLine("pong");
+      }
+      else {
+        Console.WriteLine(i);
+      }
+    }
+  }  
+}
